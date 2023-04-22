b6d4c969e1f64fadd87540a6f302c575671455a5
bduha184 <bduha_184@yahoo.co.jp>
2023-04-22 20:21:07 +0900

    first commit

---
+++ resources/js/app.ts
@@ -0,0 +1,26 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import type { DefineComponent } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m.js';
+import { Ziggy } from './ziggy.js';
+
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }):any {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
